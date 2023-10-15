48978757962b6191a79ff23586dc98186a2bfd83
cjita <ajitclarence28@gmail.com>
2023-10-15 21:16:18 +0530

    cumberProject

---
+++ com.cucumber.practice.session/src/main/java/com/cucumber/practice/session/com/cucumber/practice/session/App.java
@@ -0,0 +1,13 @@
+package com.cucumber.practice.session.com.cucumber.practice.session;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ com.cucumber.practice.session/src/test/java/PageObject/LoginPage.java
@@ -0,0 +1,13 @@
+package PageObject;
+
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+
+	WebDriver ldriver;
+
+	public LoginPage(WebDriver rDriver)
+	{
+		
+	}
+}
